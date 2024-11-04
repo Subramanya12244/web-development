754029631260981b4889078719597586e2a6e200
Subramanya12244 <asubramanya370@gmail.com>
2024-11-04 11:27:54 +0530

    commit

---
+++ Day-22 Conditional statements and funtions/2 if else practice.js
@@ -0,0 +1,87 @@
+// practice Questions:
+
+// 1. Use the logical operators to find wheather the age of a person lies between 10 and 20
+
+// let age=18;
+// if(age >10 && age <20)
+// {
+//     console.log("Age is between 10 and 20");
+// }
+// else{
+//     console.log("Age is not between 10 and 20");
+// }
+
+// 2.Demonstrate the use of switch statements in js
+
+// let month=5;
+// switch(month)
+// {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     case 7:
+//         console.log("July");
+//         break;
+//     case 8:
+//         console.log("August");
+//         break;
+//     case 9:
+//         console.log("September");
+//         break;
+//     case 10:
+//         console.log("October");
+//         break;
+//     case 11:
+//         console.log("November");
+//         break;
+//     case 12:
+//         console.log("December");
+//         break;
+//     default:
+//         console.log("Invalid month");
+// }
+
+// 3. write a js prog to find wheather a number is divisible by 2 and 3
+
+// let number=18;
+// if(number%2==0 && number%3==0)
+// {
+//     console.log("it is divisible by 2 and 3");
+// }
+// else{
+    //     console.log("it is not divisible by 2 and 3");
+    // }
+
+
+
+    // 4. write a js prog to find wheather a number is divisible by 2 or 3
+
+// let number=15;
+// if(number%2==0 || number%3==0)
+// {
+//     console.log("it is divisible by 2 or 3");
+// }
+// else{
+//     console.log("it is not divisible by 2 or 3");
+// }
+
+// 5.Print "You can Drive" or "you cannot drive" based on age being greater than 18 using terrnary operator
+
+// let age=19;
+// let result=age>=18?"You can Drive":"You cannot drive";
+// console.log(result);
