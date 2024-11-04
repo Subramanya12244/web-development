7717052146872b15392f238c40c4ef68e6dcc1af
Subramanya12244 <asubramanya370@gmail.com>
2024-11-04 18:24:12 +0530

    commit

---
+++ Day-22 Conditional statements and funtions/4 loops practice.js
@@ -0,0 +1,25 @@
+// practice set on loops
+
+// problem-1:
+// write a pgm to print the marks of a student in an object using forin loops
+// let obj={
+//     'harry':98,'rohan':70,'akash':77
+// }
+// for(const key in obj) {
+//     const element = obj[key];
+//     console.log(key,element);
+// }
+
+
+// problem-2:
+// write a pgm to print the marks of a student in an object using for loops
+// let obj={
+//         'harry':98,'rohan':70,'akash':77
+//     }
+//     let keys = Object.keys(obj);
+//     for (let i = 0; i < keys.length; i++) {
+//         let student = keys[i];
+//         console.log(`${student}: ${obj[student]}`);
+//     }
+
+
